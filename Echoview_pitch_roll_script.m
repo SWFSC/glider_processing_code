b47d0de080fbd53f457157e2319ff37a2e40efff
acossio <anthony.cossio@noaa.gov>
2023-02-09 15:29:38 -0800

    added 3 files

---
+++ Echoview_pitch_roll_script.m
@@ -0,0 +1,112 @@
+% Script for writing the GPS, pitch, roll, and depth files from the Socib files
+% for use in Echoview to join glider data with acoustic data. 
+%
+% requires epoch2datenum.m
+% 2/10/2022 A Cossio initial
+% 2/9/2023 A Cossio cleaned up code and added more comments.
+
+%load SOCIB processed mat file of glider data for the survey you want. It
+%needs to contain "data_processed" data
+%load ('C:\glider_data\amlr02\SOCIB\MAT_FILES\AMLR02_data_Cali_cruisin_5_24.mat')
+
+fn = 'amlr01'; % Change glider name as needed
+
+yrs = datestr(epoch2datenum(data_processed.time),'yyyymmdd') ; %date string for depth files
+
+dts = datestr(epoch2datenum(data_processed.time),'mm/dd/yyyy'); % date string for pitch and roll files
+dts4 = datestr(epoch2datenum(data_processed.time),'yyyy-mm-dd'); % date string for GPS files
+
+tmss = datestr(epoch2datenum(data_processed.time),'HH:MM:SS'); % time for GPS, Pitch and Roll files
+tms2 = datestr(epoch2datenum(data_processed.time),'HHMMSS'); % time for depth files
+
+tms3 = [tms2 repmat('0000',length(tms2),1)]; % make sure that time for GPS is 10 digits
+pitchdeg = rad2deg(data_processed.pitch); % convert pitch radians to degrees
+rolldeg = rad2deg(data_processed.roll); % convert roll radians to degree
+
+rep3 = repmat(3,length(tms2),1); % column of 3 for depth file. 3 indicates good line
+
+%Write GPS data for Echoview
+gps_table = table(dts4, tmss, data_processed.latitude, data_processed.longitude, 'VariableNames',{'GPS_date','GPS_time', 'Latitude', 'Longitude'} );
+gps_table = rmmissing(gps_table); % remove missing values and NaNs
+
+%write one very large csv file of GPS data for Echoview
+% writetable(gps_table,sprintf('%sALL.gps.csv',fn),'FileType','text') 
+
+%Write pitch table for Echoview
+pitch_table = table(dts, tmss, pitchdeg, 'VariableNames',{'Pitch_date','Pitch_time', 'Pitch_angle'} );
+pitch_table = rmmissing(pitch_table); % remove missing values and NaNs
+
+%write one very large csv file of pitch data for Echoview
+% writetable(pitch_table,sprintf('%sALL.pitch.csv',fn),'FileType','text')  %Pitch_date,Pitch_time,Pitch_angle
+
+%Write roll table for Echoview
+roll_table = table(dts, tmss, rolldeg,  'VariableNames',{'Roll_date','Roll_time', 'Roll_angle'}  );
+roll_table = rmmissing(roll_table); % remove missing values and NaNs
+
+%write one very large csv file of roll data for Echoview
+% writetable(roll_table,sprintf('%sALL.roll.csv',fn), 'FileType','text')
+
+%Write depth data for Echoview
+depth_table = table(yrs, tms3, data_processed.depth, rep3);  %add a column of 3
+depth_table = rmmissing(depth_table); % remove missing values and NaNs
+
+%write one very large csv file of depth data for Echoview
+% writetable(depth_table,sprintf('%sALL.depth.evl',fn),'FileType','text','Delimiter','tab')   
+
+%Copy this header then below is the count of entries must equal that number
+%below. 6070 would be 6070 entries
+% TODO add this to header of the writetable
+%EVBD 3 8.0.73.30735			
+% 6070	
+
+
+
+% GPS tables written to csv files in 500000 chunk files
+
+X = 1:500000:size(gps_table,1);
+for i = 1:length(X)
+ if i < length(X)
+writetable(gps_table(X(i):X(i+1)-1,:), sprintf('%sEV%d.gps.csv',fn, i), 'FileType', 'text')
+ else
+writetable(gps_table(X(i):end,:), sprintf('%sEV%d.gps.csv',fn, i), 'FileType', 'text')
+ end
+end
+
+
+% Roll tables written to csv filesin 500000 chunk files
+
+X = 1:500000:size(roll_table,1);
+for i = 1:length(X)
+ if i < length(X)
+writetable(roll_table(X(i):X(i+1)-1,:), sprintf('%sEV%d.roll.csv',fn, i), 'FileType', 'text')
+ else
+writetable(roll_table(X(i):end,:), sprintf('%sEV%d.roll.csv',fn, i), 'FileType', 'text')
+ end
+end
+
+
+% Pitch tables written to csv files in 500000 chunk files
+
+X = 1:500000:size(pitch_table,1);
+for i = 1:length(X)
+ if i < length(X)
+writetable(pitch_table(X(i):X(i+1)-1,:), sprintf('%sEV%d.pitch.csv',fn, i), 'FileType', 'text')
+ else
+writetable(pitch_table(X(i):end,:), sprintf('%sEV%d.pitch.csv',fn, i), 'FileType', 'text')
+ end
+end
+
+
+% Depth table written to csv files in 500000 chunk files
+
+X = 1:500000:size(depth_table,1);
+for i = 1:length(X)
+ if i < length(X)
+writetable(depth_table(X(i):X(i+1)-1,:), sprintf('%sEV%d.depth.evl',fn, i), 'FileType','text','Delimiter','tab')
+ else
+writetable(depth_table(X(i):end,:), sprintf('%sEV%d.depth.evl',fn, i), 'FileType','text','Delimiter','tab')
+ end
+end
+%EVBD 3 8.0.73.30735	%Copy this header then below is the count of entries must equal the number below.		
+% 6070	
+
---
+++ seaabsspeed.m
@@ -0,0 +1,81 @@
+function [Freq,seaAbs, seaC] = seaabsspeed(socib,date_ST, date_ED)
+
+%Get absorption coefficent and sound speed from Slocum glider processed mat
+%file created using the SOCIB Matlab code. Put in a start and end date (ie 20180625)and  
+%it will get the mean Temp, Salinity, Pressure, and Conductivity. Uses
+%formulas from ASL code (LoadAZFP.m) to calculate SS and absorption.
+% A Cossio 10/24/2018
+% A Cossio 5/12/22 added frequencies from the Nortek
+% A Cossio 2/9/23 added AZFP frequencies
+
+% If no inputs are given, prompt user
+if nargin == 0
+        [FileName,PathName] = uigetfile('*.mat','Pick glider socib mat file to process');
+    % If user pressed cancel, display and then exit
+    if FileName == 0
+        disp('User pressed cancel. Processing aborted.')
+        return
+    end
+end
+% Prompt for start date range.
+date_ST = input('Please enter start date (YYYYMMDD): ');
+
+% If input is empty, abort processing
+if isempty(date_ST)
+    disp('No start date entered. Processing aborted');
+    return
+end
+
+% Prompt for end date range.
+date_ED = input('Please enter end date (YYYYMMDD): ');
+
+% If input is empty, abort processing
+if isempty(date_ED)
+    disp('No end date entered. Processing aborted');
+    return
+end
+
+socib = fullfile(PathName,FileName); % puts the path and file name together
+load(socib, 'data_processed') % load the SOCIB mat file
+
+yrs = datestr(epoch2datenum(data_processed.time),'yyyymmdd') ; %get out the date and make it a number
+dte = str2num(yrs); %convert from string to number
+
+
+mydata = [dte data_processed.temperature data_processed.conductivity data_processed.salinity data_processed.depth];
+
+bb = mydata(:,1) > date_ST & mydata(:,1) < date_ED; % find data between 
+%subset of data based on date range
+mydata01=mydata(bb,:);
+
+T = mean(mydata01(:,2),'omitnan'); %mean temperature
+C = mean(mydata01(:,3),'omitnan'); %mean conductivity
+S = mean(mydata01(:,4),'omitnan'); %mean salinity
+P = mean(mydata01(:,5),'omitnan'); %mean depth
+
+%function seaAbs = computeAbs(T,P,S,Freq)
+Freq = [38 67 70 74 82 91 99 108 120 125];
+% calculate relaxation frequencies
+T_K = T + 273.0;
+f1 = 1320.0*(T_K)*exp(-1700/T_K);
+f2 = (1.55e7)*T_K*exp(-3052/T_K);
+
+% coefficients for absorption equations
+k = 1 + P/10.0;
+a = (8.95e-8)*(1+T*((2.29e-2)-(5.08e-4)*T));
+b = (S/35.0)*(4.88e-7)*(1+0.0134*T)*(1-0.00103*k+(3.7e-7)*(k*k));
+c = (4.86e-13)*(1+T*((-0.042)+T*((8.53e-4)-T*6.23e-6)))*(1+k*(-(3.84e-4)+k*7.57e-8));
+freqk = Freq*1000;
+seaAbs = (a*f1*(freqk.^2))./((f1*f1)+(freqk.^2))+(b*f2*(freqk.^2))./((f2*f2)+(freqk.^2))+c*(freqk.^2);
+%end
+
+% sound speed calcuation from LoadAZFP.m
+%function seaC = computeSS(T,P,S)
+
+z = T/10;
+seaC = 1449.05+z*(45.7+z*((-5.21)+0.23*z))+(1.333+z*((-0.126)+z*0.009))*(S-35.0)+(P/1000)*(16.3+0.18*(P/1000));
+T
+Freq
+seaAbs
+seaC
+%end
